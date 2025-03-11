547a16e93bd73817da15b2f67b6dfb593fd94173
aarijimam <35100854+aarijimam@users.noreply.github.com>
2025-03-12 00:22:04 +0500

    feat: added table creation queries

---
+++ drop_table.sql
@@ -0,0 +1,11 @@
+DROP TABLE IF EXISTS 
+    Study_Researcher,
+    Study_Patient,
+    PatientAllergies,
+    Researcher,
+    Study,
+    Staff,
+    Patient,
+    Allergy,
+    Hospital 
+CASCADE;
