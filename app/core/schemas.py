1ac380677ceaf6bdd757ed2f02699c97916273a9
aarijimam <35100854+aarijimam@users.noreply.github.com>
2025-03-12 12:19:28 +0500

    chore: add schema descriptions

--- app/core/schemas.py
+++ app/core/schemas.py
@@ -1,16 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import date
 from typing import Optional, List
 
 class AllergyBase(BaseModel):
-    allergyname: str
-    type: str
+    allergyname: str = Field(..., description="The name of the allergy", example="Peanuts")
+    type: str = Field(..., description="The type of the allergy", example="Food")
 
 class AllergyCreate(AllergyBase):
     pass
 
 class AllergyResponse(AllergyBase):
-    allergyid: int
+    allergyid: int = Field(..., description="The unique identifier of the allergy", example=1)
 
     class ConfigDict:
         from_attributes = True
